3b852f984850a2283e65a6580225a4402dd83bf0
Debora Gois Torres <debora@torresetorres.com>
2024-02-09 00:18:20 -0300

    Exercicio is/unless

---
+++ Ex3.rb
@@ -0,0 +1,5 @@
+puts "Whats your favorite musical genre?"
+genre = gets.chomp
+
+puts "You have a good musical taste" if genre == "rock" 
+puts "You don't have a good musical taste" unless genre == "rock" 
